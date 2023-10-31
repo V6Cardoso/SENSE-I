732268183a57e84226fe0d939be9b9eb9161b731
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2023-10-31 14:29:41 -0300

    api orion request with d3.js graphs

--- dmie/board.py
+++ dmie/board.py
@@ -5,7 +5,12 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
+from flask import current_app
 from werkzeug.exceptions import abort
+
+
+import requests
+import json
 
 from dmie.auth import login_required
 from dmie.db import get_db
@@ -25,5 +30,26 @@ def dashboard():
 @bp.route("/about")
 def about():
     return render_template("board/about.html")
+
+
+
+@bp.route("/getOrionData", methods=["POST"])
+def getOrionData():
+    url = "http://20.195.208.173:1026/v2/entities"
+    headers = {
+        'fiware-service': 'smart',
+        'fiware-servicepath': '/',
+        'Accept': 'application/json'
+    }
+    response = requests.request("GET", url, headers=headers)
+    print(response)
+    return response.json()
+    
+
+
+
+@bp.route('/service-worker.js', methods=['GET'])
+def sw():
+    return current_app.send_static_file('service-worker.js')
 
 
--- dmie/static/board/board1.js
+++ dmie/static/board/board1.js
@@ -1,178 +1,201 @@
-// Sample data (time and initial temperature)
-let data = [
-    { time: "00:00", temperature: 20 },
-    { time: "01:00", temperature: 21 },
-    { time: "02:00", temperature: 22 },
-    // Add more data points here
-];
-
 // Set the dimensions of the chart
-const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-const width = 600 - margin.left - margin.right;
+const margin = { top: 20, right: 30, bottom: 30, left: 50 };
+const width = 650 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-// Create an SVG container
-const svg = d3.select("#chart-container")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+const grid = document.querySelector('.grid');
 
-// Create scales for x and y axes
-const xScale = d3.scaleBand()
-    .domain(data.map(d => d.time))
-    .range([0, width])
-    .padding(0.1);
-
-const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.temperature)])
-    .nice()
-    .range([height, 0]);
-
-// Create x and y axes
-svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
-
-svg.append("g")
-    .attr("class", "y-axis")
-    .call(d3.axisLeft(yScale));
-
-// Create initial bars for the chart
-svg.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", d => xScale(d.time))
-    .attr("y", d => yScale(d.temperature))
-    .attr("width", xScale.bandwidth())
-    .attr("height", d => height - yScale(d.temperature))
-    .attr("fill", "steelblue");
-
-// Function to update the chart with new data
-function updateChart() {
-    // Simulate new temperature data (replace with actual data retrieval)
-    data = data.map(d => ({
-        time: d.time,
-        temperature: Math.random() * 10 + 20 // Random temperature between 20 and 30
-    }));
-
-    // Update the yScale domain with new data
-    yScale.domain([0, d3.max(data, d => d.temperature)]);
-
-    // Update the bars with new data
-    svg.selectAll(".bar")
-        .data(data)
-        .transition()
-        .duration(500) // Animation duration
-        .attr("y", d => yScale(d.temperature))
-        .attr("height", d => height - yScale(d.temperature));
+function Device(id, humidity, temperature, tempHumidity, tempTemperature) {
+  this.id = id;
+  this.humidity = humidity;
+  this.temperature = temperature;
+  this.tempHumidity = tempHumidity;
+  this.tempTemperature = tempTemperature;
 }
 
-// Call the updateChart function every second
-setInterval(updateChart, 1000);
+//compare two Devices
+Device.prototype.equals = function (device) {
+  return this.tempHumidity == device.tempHumidity && this.tempTemperature == device.tempTemperature;
+}
+
+
+let allData = {};
+
+let svg = {};
+
+
+//get data from Orion every second
+function getData() {
+  getOrionData();
+
+  //check if data is type of array
+
+
+  if (!Array.isArray(data) || data.length == 0)
+    return;
+
+  data.forEach(element => {
+    element.id = element.id.split(':')[2];
+
+    //let tempHumidity = new Date(element?.humidity?.metadata?.TimeInstant?.value);
+    //let tempTemperature = new Date(element?.temperature?.metadata?.TimeInstant?.value);
+    let tempHumidity = Date.now();
+    let tempTemperature = Date.now();
+    let humidity = element?.humidity?.value;
+    let temperature = element?.temperature?.value;
+
+    let device = new Device(element.id, humidity, temperature, tempHumidity, tempTemperature);
+
+    //check if element.id is key in allData
+    if (allData[element.id] != undefined) {
+      //keeps only the last 10 values
+      allData[element.id].push(device);
+      if (allData[element.id].length > 10) {
+        allData[element.id].shift();
+      }
+
+    }
+    else {
+      allData[element.id] = [];
+      allData[element.id].push(device);
+
+      const div = document.createElement('div');
+      div.classList.add('container');
+      const h3 = document.createElement('h3');
+      h3.innerHTML = element.id;
+      const p = document.createElement('p');
+      p.innerHTML = 'Temperatura';
+      const divChart = document.createElement('div');
+      divChart.id = element.id;
+      div.appendChild(h3);
+      div.appendChild(p);
+      div.appendChild(divChart);
+      grid.appendChild(div);
+        
+      svg[element.id] = d3.select(`#${element.id}`)
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr("class", "x label")
+        .append("g")
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+      const divHumidity = document.createElement('div');
+      divHumidity.classList.add('container');
+      const h3Humidity = document.createElement('h3');
+      h3Humidity.innerHTML = element.id;
+      const pHumidity = document.createElement('p');
+      pHumidity.innerHTML = 'Umidade';
+      const divChartHumidity = document.createElement('div');
+      divChartHumidity.id = element.id + 'h';
+      divHumidity.appendChild(h3Humidity);
+      divHumidity.appendChild(pHumidity);
+      divHumidity.appendChild(divChartHumidity);
+      grid.appendChild(divHumidity);
+        
+
+
+      svg[element.id + 'h'] = d3.select(`#${element.id}h`)
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append("g")
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    }
+    updateGraph();
+  });
+
+  //console.log(JSON.stringify(allData));
+
+
+}
+
+setInterval(getData, 1000);
 
 
 
 
 
+function updateGraph() {
+  // Iterate over each element in the 'svg' object
+  console.log(allData);
+  for (let id in svg) {
+      let auxId = id;
+      if (id[id.length - 1] == 'h')
+        auxId = id.substring(0, id.length - 1);
 
 
+      // Remove the existing path
+      svg[id].selectAll("path").remove();
+      svg[id].selectAll("g").remove();
+
+      // Define the scales
+      const xScale = d3.scaleTime().range([0, width]);
+      const yScale = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+      const line = d3.line();
+
+      // Set the domains of the scales
+      if (auxId == id) {
+        xScale.domain(d3.extent(allData[auxId], function(d) { return d.tempTemperature; }));
+        //yScale.domain([0, d3.max(allData[auxId], function(d) { return d.temperature; })]);
+        line.x(function(d) { return xScale(d.tempTemperature); }) // replace 'd.x' with your actual x-value
+        .y(function(d) { return yScale(d.temperature); }); // replace 'd.y' with your actual y-value
+      }
+      else {
+        xScale.domain(d3.extent(allData[auxId], function(d) { return d.tempHumidity; }));
+        //yScale.domain([0, d3.max(allData[auxId], function(d) { return d.humidity; })]);
+        line.x(function(d) { return xScale(d.tempHumidity); }) // replace 'd.x' with your actual x-value
+        .y(function(d) { return yScale(d.humidity); }); // replace 'd.y' with your actual y-value
+      }
+          
+
+      // Create the axis generators
+      const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%H:%M:%S"));
+      const yAxis = d3.axisLeft(yScale);
+
+      if (auxId == id) {
+        svg[id].append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 10)
+        .attr("x",0 - (height / 2) - 20)
+        .attr("dy", "0.5em")
+        .style("text-anchor", "middle")
+        .text("Temperatura");
+      }
+      else {
+        svg[id].append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 10)
+        .attr("x",0 - (height / 2) - 20)
+        .attr("dy", "0.5em")
+        .style("text-anchor", "middle")
+        .text("Umidade");
+      }
 
 
+      // Append the x-axis to the SVG
+      svg[id].append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+      // Append the y-axis to the SVG
+      svg[id].append("g")
+        .call(yAxis);
 
 
+      
+
+      // Append the path and set the 'd' attribute
+      svg[id].append("path")
+          .datum(allData[auxId])
+          .attr("fill", "none")
+          .attr("stroke", "steelblue")
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-linecap", "round")
+          .attr("stroke-width", 1.5)
+          .attr("d", line);
+  }
+}
 
 
-
-
-
-
-
-
-
-// append the svg object to the body of the page
-var svg2 = d3.select("#chart-container2")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-
-  // When reading the csv, I must format variables:
-  function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-  },
-
-  // Now I can use this dataset:
-  function(data) {
-
-    // Add X axis --> it is a date format
-    var x = d3.scaleTime()
-      .domain(d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, width ]);
-    svg2.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.value; })])
-      .range([ height, 0 ]);
-    svg2.append("g")
-      .call(d3.axisLeft(y));
-
-    // Add the line
-    svg2.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-
-})
-
-
-
-
-var svg3 = d3.select("#chart-container3")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-
-//basic api request
-/* d3.json("http://20.226.95.43:1026/v2/entities", function(data) {
-  
-}); */
-
-
-var svg4 = d3.select("#chart-container4")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-
-var svg5 = d3.select("#chart-container5")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
