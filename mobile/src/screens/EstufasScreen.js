dfe331f55cfd17a1f42c1c6b4d777ef3bd3d7b01
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2024-05-10 19:57:58 -0300

    feat: Add EstufaBottomSheetComponent for displaying estufa details

--- mobile/src/screens/EstufasScreen.js
+++ mobile/src/screens/EstufasScreen.js
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useCallback, useMemo, useRef, Suspense } from 'react';
+
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
+import { View, Text, StyleSheet, FlatList, Pressable, TouchableOpacity } from "react-native";
 import * as Progress from 'react-native-progress';
 
 import styles from '../utils/styles';
 import EstufaComponent from "../components/estufa";
+import EstufaBottomSheetComponent from '../components/EstufaBottomSheetComponent';
 
 import { getDevices, getOrionData } from "../utils/fetchData";
 
 import { connect } from "react-redux";
 import { setDevicesList } from "../../context/actions/deviceActions";
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetView, BottomSheetScrollView } from '@gorhom/bottom-sheet';
+
+import { Canvas } from '@react-three/fiber/native';
+import { EstufaModel } from '../components/EstufaModel';
+
 const EstufasScreen = (props) => {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   const [estufas, setEstufas] = useState([]);
-  let [showModal, setShowModal] = useState(false);
 
-  const closeModalHandler = () => {
-    setShowModal(false);
-  };
+  const [selectedEstufa, setSelectedEstufa] = useState(null);
+  const [selectedEstufaValue, setSelectedEstufaValue] = useState([]);
 
-  const dispatchPressEvent = () => {
-    setShowModal(true);
-  };
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => ['30%', '60%'], []);
+  const handleCloseAction = () => bottomSheetRef.current?.close();
+  const handlePresentModalPress = () => bottomSheetRef.current?.expand();
+
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index);
+  }, []);
 
   useEffect(() => {
     console.log("EstufasScreen mounted");
@@ -58,6 +69,22 @@ const EstufasScreen = (props) => {
       });
   }
 
+  useEffect(() => {
+    if (!selectedEstufa) {
+      return;
+    }
+
+    let data = estufas?.filter((item) => item.name === selectedEstufa);
+    if (!data) {
+      return;
+    }
+    else
+      data = data[0];
+    
+    setSelectedEstufaValue(data);
+    
+  }, [selectedEstufa, estufas]);
+
   const estufasExtractor = (data) => {
     if(!data)
       return;
@@ -74,44 +101,74 @@ const EstufasScreen = (props) => {
     });
   }
 
+  const handleEstufaPress = (estufa) => {
+    setSelectedEstufa(estufa.name);
+    handlePresentModalPress();
+  }
+
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Estufas</Text>
-      {isLoading && !error &&
-        <View style={style.centeredContent}>
-          <Progress.CircleSnail color={['#4682b4']} size={80} />
-          <Text style={{marginTop: 10}}>Carregando dados</Text>
-        </View>
-      }
-      {error && 
-        <View style={style.centeredContent}>
-          <Text style={{color: 'red'}}>Ocorrreu um erro ao carregar os dados</Text>
-        </View>
-      }
-      {estufas?.length !== 0 && (
-        <FlatList
-          style={style.list}
-          data={estufas}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <EstufaComponent estufa={item} onPress={dispatchPressEvent} />
-          )}
-        />
-      )}
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Text style={styles.header}>Estufas</Text>
+        {estufas?.length == 0 && isLoading && !error &&
+          <View style={style.centeredContent}>
+            <Progress.CircleSnail color={['#F5FCFF']} size={80} />
+            <Text style={{marginTop: 10, color: '#F5FCFF'}}>Carregando dados</Text>
+          </View>
+        }
+        {error &&
+          <View style={style.centeredContent}>
+            <Text style={{color: 'red'}}>Ocorrreu um erro ao carregar os dados</Text>
+          </View>
+        }
+
+        {estufas?.length !== 0 && !error && (
+          <FlatList
+            style={style.list}
+            data={estufas}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <EstufaComponent estufa={item} onPress={() => handleEstufaPress(item)} />
+            )}
+          />
+        )}
+      </View>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+        backgroundStyle={{ backgroundColor: '#F5FCFF' }}
+        enablePanDownToClose={true}
+
+      >
+        <BottomSheetScrollView contentContainerStyle={style.contentContainer}>
+          <EstufaBottomSheetComponent value={selectedEstufaValue} />
+        </BottomSheetScrollView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 };
 
 const style = StyleSheet.create({
+  
   list: {
-    width: "100%",
-    height: "100%",
+    flex: 1,
   },
   centeredContent: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  sheetContainer: {
+    
+    
+
+  },
+  contentContainer: {
+    alignItems: 'center',
+
   },
 });
 
