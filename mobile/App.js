98dde8c94c45b43b717b018056d20f4cdb8e6484
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2024-05-10 19:59:35 -0300

    feat: Update StatusBar style to "light" in App.js

--- mobile/App.js
+++ mobile/App.js
@@ -73,7 +73,7 @@ export default function App() {
       />
       </Tab.Navigator>
     </NavigationContainer>
-    <StatusBar style="auto" />
+    <StatusBar style="light" />
     </Provider>
   );
 }
